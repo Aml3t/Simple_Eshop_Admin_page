6394bbad35f428e0ff69b47ccbcc84d597add721
Aml3t <fischerparsos@gmail.com>
2024-02-22 14:05:40 +0200

    Editing the classes

--- Simple_Eshop_Admin_Page/Models/Pie.cs
+++ Simple_Eshop_Admin_Page/Models/Pie.cs
@@ -1,8 +1,28 @@
-﻿namespace Simple_Eshop_Admin_Page.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Simple_Eshop_Admin_Page.Models
 {
     public class Pie
     {
         public int PieId { get; set; }
+
+        [Display(Name = "Name")]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string? ShortDescription { get; set; }
+
+        [StringLength(1000)]
+        public string? LongDescription { get; set; }
+
+        [StringLength(1000)]
+        public string? AllergyInformation { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? ImageUrl { get; set; }
+
 
     }
 }
