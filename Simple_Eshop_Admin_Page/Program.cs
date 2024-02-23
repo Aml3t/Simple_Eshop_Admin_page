3a501d56377f9c54144d62938ed48aec9b42affa
Aml3t <fischerparsos@gmail.com>
2024-02-23 14:41:58 +0200

    Configuring EF Core

--- Simple_Eshop_Admin_Page/Program.cs
+++ Simple_Eshop_Admin_Page/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString
     ("BethanysPieShopDbContextConnection")));
 
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
