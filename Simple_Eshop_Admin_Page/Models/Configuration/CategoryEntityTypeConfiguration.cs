c92a5851caa4f621d4199327651c835452db3ff6
Aml3t <fischerparsos@gmail.com>
2024-02-23 13:01:22 +0200

    Configuring EF Core

---
+++ Simple_Eshop_Admin_Page/Models/Configuration/CategoryEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Simple_Eshop_Admin_Page.Models.Configuration
+{
+    public class CategoryEntityTypeConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
