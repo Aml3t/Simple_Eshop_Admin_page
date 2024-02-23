b3e5e0c024db98182f755f2616ce637f0465cf98
Aml3t <fischerparsos@gmail.com>
2024-02-23 12:11:04 +0200

    Configuring EF Core

---
+++ Simple_Eshop_Admin_Page/Models/BethanysPieShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Simple_Eshop_Admin_Page.Models
+{
+    public class BethanysPieShopDbContext : DbContext
+    {
+        public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext>
+            options) : base(options)
+        {
+            
+        }
+    }
+}
