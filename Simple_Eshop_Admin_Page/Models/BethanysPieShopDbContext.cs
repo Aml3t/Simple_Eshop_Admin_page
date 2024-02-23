f97233cb00caf9f2c8ebc579cb23b3a0dbdb99d9
Aml3t <fischerparsos@gmail.com>
2024-02-23 12:20:24 +0200

    Configuring EF Core

--- Simple_Eshop_Admin_Page/Models/BethanysPieShopDbContext.cs
+++ Simple_Eshop_Admin_Page/Models/BethanysPieShopDbContext.cs
@@ -14,5 +14,15 @@ namespace Simple_Eshop_Admin_Page.Models
         public DbSet<Pie> Pies { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
+
+
+        //This is how we use different name for our Objects to the database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().ToTable("Categories");
+            modelBuilder.Entity<Pie>().ToTable("Pies");
+            modelBuilder.Entity<Order>().ToTable("Orders");
+            modelBuilder.Entity<OrderDetail>().ToTable("OrderDetails");
+        }
     }
 }
