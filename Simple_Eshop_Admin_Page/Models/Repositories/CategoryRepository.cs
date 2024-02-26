6b8250409b87986b3fb244fdb0155c03b69a5a69
Aml3t <fischerparsos@gmail.com>
2024-02-26 13:04:16 +0200

    Configuring repos

--- Simple_Eshop_Admin_Page/Models/Repositories/CategoryRepository.cs
+++ Simple_Eshop_Admin_Page/Models/Repositories/CategoryRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace Simple_Eshop_Admin_Page.Models.Repositories
+﻿
+namespace Simple_Eshop_Admin_Page.Models.Repositories
 {
     public class CategoryRepository : ICategoryRepository
     {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+
+        public CategoryRepository(BethanysPieShopDbContext bethanysPieShopDbContext)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext;
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _bethanysPieShopDbContext.Categories.OrderBy(o => o.CategoryId);
+        }
+
+        public Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
