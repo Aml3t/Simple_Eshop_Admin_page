6108454bfc84fd571623ea16cbf21d8ba988b829
Aml3t <fischerparsos@gmail.com>
2024-02-26 14:02:26 +0200

    Configuring repos

---
+++ Simple_Eshop_Admin_Page/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Simple_Eshop_Admin_Page.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
