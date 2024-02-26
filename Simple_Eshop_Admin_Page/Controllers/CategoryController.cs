1f8364a5e80417c94e7582c999d1fc2d23f60d8d
Aml3t <fischerparsos@gmail.com>
2024-02-26 14:45:57 +0200

    Configuring repos

--- Simple_Eshop_Admin_Page/Controllers/CategoryController.cs
+++ Simple_Eshop_Admin_Page/Controllers/CategoryController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Simple_Eshop_Admin_Page.Models;
+using Simple_Eshop_Admin_Page.Models.Repositories;
 
 namespace Simple_Eshop_Admin_Page.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
         public IActionResult Index()
         {
             return View();
