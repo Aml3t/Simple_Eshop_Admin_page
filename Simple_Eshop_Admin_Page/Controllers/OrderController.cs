2841ef7b7c9fb14c62f9ec3e93556c67a0ec3869
Aml3t <fischerparsos@gmail.com>
2024-02-29 10:53:54 +0200

    Configuring Order pages

--- Simple_Eshop_Admin_Page/Controllers/OrderController.cs
+++ Simple_Eshop_Admin_Page/Controllers/OrderController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Simple_Eshop_Admin_Page.Models.Repositories;
 
 namespace Simple_Eshop_Admin_Page.Controllers
 {
     public class OrderController : Controller
     {
-        
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
     }
 }
