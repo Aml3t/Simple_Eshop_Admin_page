3bc01000cb585b3892ad474096993411a7032731
Aml3t <fischerparsos@gmail.com>
2024-02-29 11:01:55 +0200

    Configuring Order pages

--- Simple_Eshop_Admin_Page/ViewModels/OrderIndexViewModel.cs
+++ Simple_Eshop_Admin_Page/ViewModels/OrderIndexViewModel.cs
@@ -6,7 +6,11 @@ namespace Simple_Eshop_Admin_Page.ViewModels
 {
     public class OrderIndexViewModel
     {
-        public List<Order>? Orders { get; set; }
+        public IEnumerable<Order>? Orders { get; set; }
+        public IEnumerable<OrderDetail>? OrderDetails { get; set; }
+        public IEnumerable<Pie>? Pies { get; set; }
+        public int? SelectedOrderId { get; set; }
+        public int? SelectedOrderDetailId { get; set; }
 
     }
 }
