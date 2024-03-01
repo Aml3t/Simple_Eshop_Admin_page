edf013eb0026111544269ed195158de6d96d44f9
Aml3t <fischerparsos@gmail.com>
2024-03-01 11:08:35 +0200

    Creating Add Pie page

--- Simple_Eshop_Admin_Page/Controllers/PieController.cs
+++ Simple_Eshop_Admin_Page/Controllers/PieController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Simple_Eshop_Admin_Page.Models;
 using Simple_Eshop_Admin_Page.Models.Repositories;
 
 namespace Simple_Eshop_Admin_Page.Controllers
@@ -6,10 +7,12 @@ namespace Simple_Eshop_Admin_Page.Controllers
     public class PieController : Controller
     {
         private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public PieController(IPieRepository pieRepository)
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
         {
             _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
         }
 
         public async Task<IActionResult> Index()
@@ -26,7 +29,13 @@ namespace Simple_Eshop_Admin_Page.Controllers
             return View(pie);
         }
 
-        
+        public async Task<IActionResult> Add()
+        {
+            var allCategories = await _categoryRepository.GetAllCategoriesAsync();
+
+            var pie = _pieRepository.AddPieAsync(new Pie { });
+
+        }
 
     }
 }
