76e3318f1a198059b8e41dc1ae300bd531ca8bcd
Aml3t <fischerparsos@gmail.com>
2024-03-04 12:30:38 +0200

    Creating Validation with model state

--- Simple_Eshop_Admin_Page/Models/Pie.cs
+++ Simple_Eshop_Admin_Page/Models/Pie.cs
@@ -11,33 +11,39 @@ namespace Simple_Eshop_Admin_Page.Models
         public string Name { get; set; } = string.Empty;
 
         [StringLength(100)]
-        [Display(Name ="Short Description")]
+        [Display(Name = "Short description")]
         public string? ShortDescription { get; set; }
 
+        [Display(Name = "Long description")]
         [StringLength(1000)]
-        [Display(Name = "Lost Description")]
         public string? LongDescription { get; set; }
 
-        [StringLength(1000)]
         [Display(Name = "Allergy information")]
+        [StringLength(1000)]
         public string? AllergyInformation { get; set; }
 
         [Display(Name = "Price")]
         public decimal Price { get; set; }
 
+        [Display(Name = "Image URL")]
         public string? ImageUrl { get; set; }
 
+        [Display(Name = "Image thumbnail URL")]
         public string? ImageThumbnailUrl { get; set; }
 
+        [Display(Name = "Is pie of the week?")]
         public bool IsPieOfTheWeek { get; set; }
 
+        [Display(Name = "In stock?")]
         public bool InStock { get; set; }
 
+        [Display(Name = "Category ID")]
         public int CategoryId { get; set; }
 
+        [Display(Name = "Category")]
         public Category? Category { get; set; }
 
+        [Display(Name = "Ingredients")]
         public ICollection<Ingredient>? Ingredients { get; set; }
-
     }
 }
