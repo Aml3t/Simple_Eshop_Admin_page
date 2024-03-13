eb493835cf94183e2f2566fa6b70d1d6f74b3be3
Aml3t <fischerparsos@gmail.com>
2024-03-13 11:13:51 +0200

    Adding Paging

--- Simple_Eshop_Admin_Page/Utilities/PagedList.cs
+++ Simple_Eshop_Admin_Page/Utilities/PagedList.cs
@@ -1,7 +1,17 @@
-﻿namespace Simple_Eshop_Admin_Page.Utilities
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Simple_Eshop_Admin_Page.Utilities
 {
     public class PagedList<T> : List<T>
     {
+        public int PageIndex { get; private set; }
+        public int TotalNumberOfPages { get; private set; }
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalNumberOfPages;
 
+        public PagedList(List<T> items, int count, int pageIndex,  )
+        {
+            
+        }
     }
 }
