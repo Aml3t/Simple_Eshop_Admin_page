9a622204627917086d14eb7ddb1e0248f1e83a4b
Aml3t <fischerparsos@gmail.com>
2024-03-20 13:27:42 +0200

    Adding entities

---
+++ Simple_Eshop_Admin_Page/ViewModels/CategoryBulkEditViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Simple_Eshop_Admin_Page.Models;
+
+namespace Simple_Eshop_Admin_Page.ViewModels
+{
+    public class CategoryBulkEditViewModel
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
