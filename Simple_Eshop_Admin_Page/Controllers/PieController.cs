e80693e64921fbcddc181a94a2dbaa96e4d64ce3
Aml3t <fischerparsos@gmail.com>
2024-03-21 12:40:36 +0200

    Handling Concurrency

--- Simple_Eshop_Admin_Page/Controllers/PieController.cs
+++ Simple_Eshop_Admin_Page/Controllers/PieController.cs
@@ -135,9 +135,45 @@ namespace Simple_Eshop_Admin_Page.Controllers
 
         }
 
+        //[HttpPost]
+        //public async Task<IActionResult> Edit(PieEditViewModel pieEditViewModel)
+        //{
+        //    try
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            await _pieRepository.UpdatePieAsync(pieEditViewModel.Pie);
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        else
+        //        {
+        //            return BadRequest();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        ModelState.AddModelError("", $"Updating the category failed," +
+        //            $"please try again! Error: {ex.Message}");
+        //    }
+
+        //    var allCategories = await _categoryRepository
+        //        .GetAllCategoriesAsync();
+
+        //    IEnumerable<SelectListItem> selectListItems = new SelectList
+        //        (allCategories, "CategoryId", "Name", null);
+
+        //    pieEditViewModel.Categories = selectListItems;
+
+        //    return View(pieEditViewModel);
+
+        //}
+
         [HttpPost]
-        public async Task<IActionResult> Edit(PieEditViewModel pieEditViewModel)
+        public async Task<IActionResult> Edit(Pie pie)
         {
+            Pie pieToUpdate = await _pieRepository.GetPieByIdAsync(pie.PieId);
+
+
             try
             {
                 if (ModelState.IsValid)
